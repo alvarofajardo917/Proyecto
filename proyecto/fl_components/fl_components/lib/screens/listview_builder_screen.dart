27a031227766feffa2d26e33e44c86361119880d
Yaquelin Burgos Saavedra <Yaquelin.Burgos@UPDS.edu.bo>
2024-03-25 16:12:17 -0400

    25-03-2024 16:12

--- proyecto/fl_components/fl_components/lib/screens/listview_builder_screen.dart
+++ proyecto/fl_components/fl_components/lib/screens/listview_builder_screen.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class ListViewScreen extends StatelessWidget {
+class ListViewScreen extends StatefulWidget {
   const ListViewScreen({Key? key}) : super(key: key);
 
   @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    scrollController.addListener(() {
+      //print(' ${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if((scrollController.position.pixels + 500) >= scrollController.position.maxScrollExtent){
+        adds();
+      }
+    });
+  }
+
+void adds(){
+  final lastId = imagesIds.last;
+  imagesIds.addAll(
+    [1, 2, 3, 4, 5].map((e) => lastId + e)
+  );
+  setState(() {});
+}
+
+
+  @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      context: context,
-      child: Scaffold(
-        body: ListView.builder(
-          itemCount: 100,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          physics: const BouncingScrollPhysics(), // es para que tenga un parecido a ios
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
                 width: double.infinity,
                 height: 300,
-                fit:BoxFit.cover,
+                fit: BoxFit.cover,
                 placeholder: const AssetImage('assets/jar-loading.gif'),
                 image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${index + 1}')
-            );
+                    'https://picsum.photos/500/300?image=${imagesIds[index]}'));
           },
         ),
       ),
